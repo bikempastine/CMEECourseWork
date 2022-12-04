e9323f534359cec94f189b1cee012db070d22f49
Bikem Pastine <bp222@ac.ac.uk>
2022-12-04 16:10:33 +0000

    second last push

--- MiniProject/code/plot_models.R
+++ MiniProject/code/plot_models.R
@@ -33,7 +33,7 @@ plot_by_id <- function(df, ID){
 
 #save the results and add the model fit line to the plot, with text containing info about the graphs
 pdf(width = 12, height = 8, "../results/linear_models.pdf")
-for (ID in 1:length(d)){
+for (ID in data_cubic$ID){
     print(plot_by_id(d, ID)+ 
           geom_line(data = l[[ID]], aes(x = Time, y = PopBio), size = 1, colour = '#f8766D')+ #linaer model
           geom_line(data = q[[ID]], aes(x = Time, y = PopBio), size = 1, colour = '#7CAE00')+ #quadratic model
@@ -48,3 +48,30 @@ dev.off()
 p <- plot_by_id(d, ID)
 p <- p + geom_line(data = g[[ID]], aes(x = Time, y = N), size = 1, colour = 'purple')
 print(p)
+
+
+for(i in as.numeric(data_cubic$ID)){
+    plot_by_id(d, ID)+ 
+          geom_line(data = l[[ID]], aes(x = Time, y = PopBio), size = 1, colour = '#f8766D')+ #linaer model
+          geom_line(data = q[[ID]], aes(x = Time, y = PopBio), size = 1, colour = '#7CAE00')+ #quadratic model
+          geom_line(data = c[[ID]], aes(x = Time, y = PopBio), size = 1, colour = '#00BFC4') + #cubic model
+          geom_line(data = g[[ID]], aes(x = Time, y = N), size = 1, colour = '#C77CFF') #+ #gompertz model
+}
+
+        #   annotate("text", x = Inf, y = Inf, label = paste("AICc Linear:", signif(l_model[ID,7], 4), "\nAICc Quadratic:", 
+        #             signif(q_model[ID,8],4), "\nAICc Cubic:", signif(c_model[ID,9],4), "\nAICc Gompertz:", signif(g_model[ID, 10],4)), vjust = 6.5, hjust = 1)
+ggplot()
+
+trial <- unlist(d)
+a <- d[[1]]
+for(i in 2:length(d)){
+     a=rbind(a, d[[i]])
+}
+
+library(dplyr)
+
+a %>% filter(ID == 2) %>% ggplot( aes(x= Time,y=PopBio))+
+    geom_point(size = 2, shape =19)+
+         xlab("Time")+ ylab("logPopulation")+
+         theme_bw()+
+         #labs(subtitle = paste(" Species name:", Species, " \n Medium:",Medium, "\n Temp:", df[[ID]]$Temp, "\n Cite:",Citation, "\n ID:",ID))
