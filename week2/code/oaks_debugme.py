935d4068223cc516a57ea438cdd2d32ac57442fa
Bikem Pastine <bp222@ac.ac.uk>
2022-12-17 13:03:06 +0000

    groupwrok files in the appropriate places

--- week2/code/oaks_debugme.py
+++ week2/code/oaks_debugme.py
@@ -1,66 +1,56 @@
 #!/usr/bin/env python3
 
-"""This script checks for oaks in a csv file 'TestOaksData.csv'."""
 
-__appname__ = 'oaks_debugme.py'
-__author__ = 'Bikem Pastine (bp222@ic.ac.uk)'
+"""A practical problem on debugging of a function that determine whether a species is an oak."""
+
+__author__ = 'Dashing Dingos'
 __version__ = '0.0.1'
-__license__ = "No license needed"
 
-
-#imports
-import csv
+import csv #import csv
 import sys
 import doctest
 
 #Define function
+#import ipdb; ipdb.set_trace()
 def is_an_oak(name):
-    """ Returns True if name is starts with 'quercus' 
 
-    >>> is_an_oak('Fagus sylvatica')
+    """ Returns True if name is starts with 'quercus'
+    >>> is_an_oak('Fagus sylvatica') #wrong name
     False
-
-    >>> is_an_oak('Quercus petraea')
+    >>> is_an_oak('Quercus robur') #Capital letter
     True
-
-    >>> is_an_oak('QUERCUS PETRAEA')
+    >>> is_an_oak('quercus robur') #correct !
     True
-
-    >>> is_an_oak('QuercusPetraea')
-    False
-
-    >>> is_an_oak('Quercuss Petraea')
-    False
-
-    >>> is_an_oak('qiercus petraea')
-    False
-
-    >>> is_an_oak('Quercus-abcd')
-    False
-
+    >>> is_an_oak('quercuss robur') #typo 
+    True
     """
-    
-    return name.lower().startswith('quercus ') #include the space between genus and species to only accept quercus
+    return name.lower().startswith('quercus')
+doctest.testmod()
 
 def main(argv): 
-    """Runs the script"""
+    """ Exclude the header row and include the column headers. """ 
     f = open('../data/TestOaksData.csv','r')
-    g = open('../data/JustOaksData.csv','w')
+    g = open('../results/JustOaksData.csv','w')
     taxa = csv.reader(f)
+    #use this to exclude the header
+    next(taxa)
     csvwrite = csv.writer(g)
     oaks = set()
     for row in taxa:
+        #print the species
         print(row)
-        print ("The genus is: ") 
-        print(row[0] + '\n')
-        if is_an_oak(row[0] + ' '): #modify the input for the function to include a space sudo-sepperating the genus and species names
-            #breakpoint()
+        print("The genus is: ")
+        print(row[0])
+        #if it is an oak
+        if is_an_oak(row[0]):
             print('FOUND AN OAK!\n')
-            csvwrite.writerow([row[0], row[1]])    
-
+        #if not
+        else:
+            print("NOT AN OAK!\n")
+            csvwrite.writerow([row[0], row[1]])
+    print("\nOaks saved to ../results/JustOaksData.csv")
     return 0
-    
+
 if (__name__ == "__main__"):
     status = main(sys.argv)
-
-doctest.testmod()
+    sys.exit(status)
--- week3/code/Vectorize1.R
+++ week3/code/Vectorize1.R
@@ -1,9 +1,12 @@
-# Script: Vectorize1.R
-# sums all elements of a matrix
+#Author: Dashing Dingos
+#Script: Vectorize1.R
+#Created: Dec 2022
+#Desc: Vectorizantion class work: edited for Groupwork practical on Vectorization
 
+# Generate random numbers
 m<- matrix(runif(1000000), 1000, 1000)
 
-
+# Loopy function
 SumAllElements <- function(m){
     Dimentions <- dim(m)
     Tot <- 0
@@ -15,8 +18,24 @@ SumAllElements <- function(m){
     return(Tot)
 }
 
-print("Using loops, the time taken is:")
-print(system.time(SumAllElements(m)))
 
-print("Using the in-built vectorised function, the time taken is:")
-print(system.time(sum(m)))
+# Do time measurement
+# 1: Time the looping function
+start_time_loops <- Sys.time()
+T <- SumAllElements(m)
+end_time_loops <- Sys.time()
+time_taken_loops <- difftime(end_time_loops, start_time_loops, units = "secs")[[1]]
+
+# Paste together the time and a string to describe it
+loop_time <- paste("R SumAllElements function:",format(time_taken_loops), "seconds" )
+print(loop_time)
+
+# 2: Time the vectorized function
+start_time_vectorized <- Sys.time()
+T<- sum(m)
+end_time_vectorized <- Sys.time()
+time_taken_vectorized <- difftime(end_time_vectorized, start_time_vectorized, units = "secs")[[1]]
+
+# Paste together the time and a string to describe it
+vectorized_time<- paste("R sum vectorized function:",format(time_taken_vectorized), "seconds" )
+print(vectorized_time)
