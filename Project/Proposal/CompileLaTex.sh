a36e33c0a924e8ee9acace6208d561a38d53146a
Bikem Pastine <bp222@ac.ac.uk>
2023-04-03 07:45:01 +0100

    Submit proposal

---
+++ Project/Proposal/CompileLaTex.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Author: Bikem bp222@ic.ac.uk
+# Script: compile LaTeX files
+# Arguments: 1 -> LaTeX file
+# Date: Oct 2022
+
+#allow imput to be full filename including extension
+filename=`echo "${1%.*}"`
+echo "$filename"
+
+#Compile file
+pdflatex $filename.tex
+bibtex $filename
+pdflatex $filename.tex
+pdflatex $filename.tex
+
+#move output to the results section and open file
+evince $filename.pdf &
+
+
+##Cleanup
+rm *.aux
+rm *.log
+rm *.bbl
+rm *.blg
+
+#exit
